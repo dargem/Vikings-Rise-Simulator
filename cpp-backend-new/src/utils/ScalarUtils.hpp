d2aa00284fb3072db428a7c54c4141cbc93bf439
dargem <genericz145@gmail.com>
2025-12-11 20:15:42 +1100

    scalar utils calc

--- cpp-backend-new/src/utils/ScalarUtils.hpp
+++ cpp-backend-new/src/utils/ScalarUtils.hpp
@@ -1,6 +1,7 @@
 #ifndef SCALARS_HPP
 #define SCALARS_HPP
 
+#include <cmath>
 #include "combatants/UnitType.hpp"
 
 namespace ScalarUtils
@@ -8,6 +9,17 @@ namespace ScalarUtils
     constexpr double TROOP_ADVANTAGE_DAMAGE_MULTIPLYER {1.05};
     constexpr double TROOP_ADVANTAGE_RECEIVED_MULTIPLYER {0.95};
     constexpr double TROOP_DEFAULT_MULTIPLYER {1};
+    
+    // 100+k gradient
+    constexpr double GRADIENT_100K_RISE { 0.001 };
+    constexpr double GRADIENT_100K_CONSTANT { 432.2219717 };
+
+    // messy estimate real value is probably 1/275 I assume?
+    constexpr double GRADIENT_10K_100K_RISE {0.003634298861};
+    constexpr double GRADIENT_10K_100K_CONSTANT {105.1350742};
+
+    constexpr double LINEAR_FACTOR_0K_10K { 0.778 };
+    constexpr double POWER_FACTOR_0K_10K { 0.487 };
 
     bool troopTypeAdvantage(UnitType friendly, UnitType target);
 
@@ -24,11 +36,17 @@ namespace ScalarUtils
         // troop scalar 
         if (troops >= 100000)
         {
-            return (troops - 100000) * 0.001 + 432.2219717;
+            return (troops - 100000) * GRADIENT_100K_RISE + GRADIENT_100K_CONSTANT;
+        }
+        else if (troops > 10000)
+        {
+            return (troops - 10000) * GRADIENT_10K_100K_RISE + GRADIENT_10K_100K_CONSTANT;
+        }
+        else
+        {
+            return LINEAR_FACTOR_0K_10K * std::pow(troops, POWER_FACTOR_0K_10K);
         }
     }
-    
-
 }
 
 #endif
