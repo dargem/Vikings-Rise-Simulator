47115d5147e6fd75665d2010f3c7b99152a4a1cc
dargem <genericz145@gmail.com>
2026-01-11 15:54:12 +1100

    status skill template so its actually got no branching, needs integration

--- cpp-backend-new/include/skills/StatusSkill.hpp
+++ cpp-backend-new/include/skills/StatusSkill.hpp
@@ -7,9 +7,9 @@
 #include "effects/TimedEffect.hpp"
 #include "effects/EffectType.hpp"
 #include "skills/Skill.hpp"
-// forward declaration for combatant to prevent a circular dependency
-class Combatant;
+#include "combatants/Combatant.hpp"
 
+template<SkillTarget Target> 
 class StatusSkill: public Skill
 {
 public:
@@ -18,7 +18,6 @@ public:
         SkillType skill_type, 
         Condition skill_condition, 
         CombatantEvent skill_dependent, 
-        SkillTarget skill_target, 
         double chance,
         EffectType effect_type, 
         bool is_removable
@@ -27,7 +26,7 @@ public:
     void onDependent(
         Combatant& self, 
         Combatant& target, 
-        NumberGenerator& number_generato
+        NumberGenerator& number_generator
     ) const override;
     
     bool operator==(const Skill& other) const override;
@@ -35,13 +34,52 @@ private:
     const TimedEffect status_effect;
     const bool is_removable;
     const EffectType effect_type;
-    const SkillTarget skill_target;
-
-    using ApplyFunction = void(*)(Combatant&, Combatant&, const TimedEffect&, EffectType);
-    ApplyFunction apply_func;
-
-    static void applyToSelf(Combatant& self, Combatant& target, const TimedEffect& effect, EffectType type);
-    static void applyToTarget(Combatant& self, Combatant& target, const TimedEffect& effect, EffectType type);
 };
+
+template<SkillTarget Target>
+StatusSkill<Target>::StatusSkill(
+    const TimedEffect status_effect, 
+    const SkillType skill_type, 
+    const Condition skill_condition, 
+    const CombatantEvent skill_dependent, 
+    const double chance,
+    const EffectType effect_type, 
+    const bool is_removable
+)
+    : Skill(skill_type, skill_condition, skill_dependent, chance),
+    status_effect(status_effect),
+    effect_type { effect_type },
+    is_removable { is_removable }
+{}
+
+template<SkillTarget Target>
+void StatusSkill<Target>::onDependent(Combatant& self, Combatant& target, NumberGenerator& number_generator) const
+{
+    if (Skill::checkCondition(self, target, number_generator))
+    {
+        const double scalars = 1;
+        TimedEffect sent_effect(status_effect, scalars);
+
+        if constexpr (Target == SkillTarget::FRIENDLY)
+        {
+            self.addStatusEffect(sent_effect, effect_type);
+        }
+        else
+        {
+            target.addStatusEffect(sent_effect, effect_type);
+        }
+    }
+}
+
+template<SkillTarget Target>
+bool StatusSkill<Target>::operator==(const Skill& other) const
+{
+    const auto* other_skill = dynamic_cast<const StatusSkill*>(&other);
+    if (other_skill != nullptr)
+    {
+        return getSkillType() == other_skill->getSkillType();
+    }
+    return false;
+}
 
 #endif
